91b14f3e2a6377b0ce61336816173236563f36e0
Eliza Brock <eliza@elizabrocksoftware.com>
2014-07-18 11:27:12 -0500

    Month#to_s now starts on correct day of week.

--- lib/month.rb
+++ lib/month.rb
@@ -1,5 +1,9 @@
+require_relative 'zellers_congruence'
+
 class Month
   MONTHS = [nil, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  WEEK_LENGTH = 7
+  DAY_WIDTH = 2
 
   def initialize(month, year)
     @month = month
@@ -17,14 +21,22 @@ class Month
   def to_s
     output = header
     output << "\nSu Mo Tu We Th Fr Sa\n"
-    output << <<EOS
-                   1
- 2  3  4  5  6  7  8
- 9 10 11 12 13 14 15
-16 17 18 19 20 21 22
-23 24 25 26 27 28 29
-30 31
-EOS
+    month_length = 31
+    full_month = Array.new(month_length){ |index| index + 1 }
+    padding.times{ full_month.unshift(nil) }
+
+    full_month.each_slice(WEEK_LENGTH) do |week|
+      days = week.map{ |day| day.to_s.rjust(DAY_WIDTH) }
+      output << days.join(" ")
+      output << "\n"
+    end
     output
+  end
+
+  private
+
+  def padding
+    zellers = ZellersCongruence.calculate(@month, @year)
+    (zellers + 6 ) % 7
   end
 end
--- spec/cal_month_integration_spec.rb
+++ spec/cal_month_integration_spec.rb
@@ -1,6 +1,37 @@
 RSpec.describe "Cal's full month integration" do
-  it "should correctly print July 2017" do
-    expected = <<EOS
+  context "Edge Cases" do
+    it "should print a month that is only 4 weeks long" do
+      expected = <<EOS
+   February 2015
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+
+
+EOS
+      actual = `./cal 02 2015`
+      actual.should == expected
+    end
+
+    it "should print a month that is 6 weeks long" do
+      expected = <<EOS
+    January 2016
+Su Mo Tu We Th Fr Sa
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+31
+EOS
+      actual = `./cal 01 2016`
+      actual.should == expected
+    end
+
+    it "should print a month that starts on Saturday" do
+      expected = <<EOS
      July 2017
 Su Mo Tu We Th Fr Sa
                    1
@@ -10,16 +41,59 @@ Su Mo Tu We Th Fr Sa
 23 24 25 26 27 28 29
 30 31
 EOS
-    # Equivalent to:
-    # expected = "     July 2017\nSu Mo Tu We Th Fr Sa\n                   1\n 2  3  4  5  6  7  8\n 9 10 11 12 13 14 15\n16 17 18 19 20 21 22\n23 24 25 26 27 28 29\n30 31\n\n"
+      actual = `./cal 07 2017`
+      actual.should == expected
+    end
 
-    # exec : Executes the command on the command line
-    actual = `./cal 07 2017`
-    actual.should == expected
+    it "should print a month that starts on Sunday" do
+      expected = <<EOS
+    January 2017
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31
+
+EOS
+      actual = `./cal 01 2017`
+      actual.should == expected
+    end
+
+    it "should print February in quad century leap years" do
+      expected = <<EOS
+   February 2000
+Su Mo Tu We Th Fr Sa
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29
+
+EOS
+      actual = `./cal 02 2000`
+      actual.should == expected
+    end
+
+    it "should print February in century non-leap years" do
+      expected = <<EOS
+   February 1900
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28
+
+EOS
+      actual = `./cal 02 1900`
+      actual.should == expected
+    end
   end
 
-  it "should correctly print December 2013" do
-    expected = <<EOS
+  context "Base Cases" do
+    it "should correctly print a standard 5 week month" do
+      expected = <<EOS
    December 2013
 Su Mo Tu We Th Fr Sa
  1  2  3  4  5  6  7
@@ -29,7 +103,83 @@ Su Mo Tu We Th Fr Sa
 29 30 31
 
 EOS
-    actual = `./cal 12 2013`
-    actual.should == expected
+      actual = `./cal 12 2013`
+      actual.should == expected
+    end
+
+    it "should print a month that begins in the middle of a week" do
+      expected = <<EOS
+    August 2013
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30 31
+
+EOS
+      actual = `./cal 08 2013`
+      actual.should == expected
+    end
+
+    it "should print 30 day months" do
+      expected = <<EOS
+   September 2013
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30
+
+EOS
+      actual = `./cal 09 2013`
+      actual.should == expected
+    end
+
+    it "should print 31 day months" do
+      expected = <<EOS
+   December 2012
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+EOS
+      actual = `./cal 12 2012`
+      actual.should == expected
+    end
+
+    it "should print 28 day months" do
+      expected = <<EOS
+   February 2003
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28
+
+EOS
+      actual = `./cal 02 2003`
+      actual.should == expected
+    end
+
+    it "should print 29 day months" do
+      expected = <<EOS
+   February 2008
+Su Mo Tu We Th Fr Sa
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29
+
+EOS
+      actual = `./cal 02 2008`
+      actual.should == expected
+    end
   end
 end
