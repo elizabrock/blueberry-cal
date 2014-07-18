cc8d93d2046032d30c71e5cf53ad1662e8801d57
Eliza Brock <eliza@elizabrocksoftware.com>
2014-07-18 10:40:50 -0500

    Implement Zeller's Congruence

---
+++ lib/zellers_congruence.rb
@@ -0,0 +1,13 @@
+class ZellersCongruence
+  def self.calculate(month, year)
+    q = 1 # day of month
+    m = (month <= 2) ? month + 12 : month
+    y = (month <= 2) ? year - 1 : year
+
+    p1 = (((m+1) * 26) / 10).floor
+    p2 = (y/4).floor
+    p3 = 6 * (y/100).floor
+    p4 = (y/400).floor
+    (q + p1 + y + p2 + p3 + p4) % 7
+  end
+end
